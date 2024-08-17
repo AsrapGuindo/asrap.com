2e54cd5a12f9b111277fae617080d31dc983a2ed
AsrapGuindo <guindoasrap@gmail.com>
2024-08-17 14:52:46 +0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,25 @@
+function l(){
+    document.getElementById("spnn").style.marginLeft="0%";
+    document.getElementById("division1").style.backgroundColor="white";
+    document.getElementById("section").style.backgroundColor="white";
+    document.getElementById("section1").style.backgroundImage="none";
+    document.getElementById("section2").style.backgroundColor="white";
+    document.getElementById("section3").style.backgroundColor="white";
+    document.getElementById("section4").style.backgroundColor="white";
+    document.getElementById("section").style.backgroundColor="white";
+    document.getElementById("nav").style.backgroundColor="white";
+  
+    
+    
+    
+}
+function d(){
+    document.getElementById("spnn").style.marginLeft="50%";
+    document.getElementById("division1").style.backgroundColor="#041A40";
+    document.getElementById("section").style.backgroundColor="#041A40";
+    document.getElementById("section1").style.backgroundImage=" url(../../Pictures/bk.jfif)";
+    document.getElementById("section2").style.backgroundColor="dimgray";
+    document.getElementById("nav").style.backgroundColor="#333";
+    document.getElementById("section3").style.backgroundColor="#37474f";
+    document.getElementById("section4").style.backgroundColor="#263238";
+}
